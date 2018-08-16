0db22d8cdd7bd62518e446890fcc369fc42d033a
Karol <kb.appsdesign@gmail.com>
2018-08-16 22:27:56 +0200

    19.4. Exercise finished.

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -25,8 +25,9 @@ public class EmailScheduler {
     //@Scheduled(fixedDelay = 10000)
     public void sendInformationEmail() {
         long size = taskRepository.count();
+        String tasks = (size == 1) ? " task." : " tasks.";
         simpleEmailService.send(new Mail(
-            adminConfig.getAdminMail(), "", SUBJECT, "Currently in database you got: " + size + " tasks."
+            adminConfig.getAdminMail(), "", SUBJECT, "Currently in database you got: " + size + tasks
         ));
     }
 
