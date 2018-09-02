b78af8fbb2243556d8aa8d52ec2846bb2d2fc903
Karol <kb.appsdesign@gmail.com>
2018-09-02 17:03:54 +0200

    21.1. Exercise finished. Test of TrelloMapper

--- src/test/java/com/crud/tasks/mapper/TrelloMapperTestSuite.java
+++ src/test/java/com/crud/tasks/mapper/TrelloMapperTestSuite.java
@@ -1,8 +1,6 @@
 package com.crud.tasks.mapper;
 
-import com.crud.tasks.domain.TrelloBoard;
-import com.crud.tasks.domain.TrelloBoardDto;
-import com.crud.tasks.domain.TrelloListDto;
+import com.crud.tasks.domain.*;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -21,20 +19,37 @@ public class TrelloMapperTestSuite {
     @Test
     public void testMapToBards() {
         //Given
-        List<TrelloListDto> lstTrelloListDto = new ArrayList<>();
-        lstTrelloListDto.add(new TrelloListDto("1", "test list", false));
-        TrelloBoardDto trelloBoardDto1 = new TrelloBoardDto("1","test board1", lstTrelloListDto);
-        TrelloBoardDto trelloBoardDto2 = new TrelloBoardDto("2","test board2", lstTrelloListDto);
-        TrelloBoardDto trelloBoardDto3 = new TrelloBoardDto("3","test board3", lstTrelloListDto);
-        List<TrelloBoardDto> lstTrelloBoardsDto = new ArrayList<>();
-        lstTrelloBoardsDto.add(trelloBoardDto1);
-        lstTrelloBoardsDto.add(trelloBoardDto2);
-        lstTrelloBoardsDto.add(trelloBoardDto3);
+        List<TrelloListDto> testLstTrelloListDto = new ArrayList<>();
+        testLstTrelloListDto.add(new TrelloListDto("1", "test list name", false));
+        TrelloBoardDto trelloBoardDto1 = new TrelloBoardDto("1","test board1", testLstTrelloListDto);
+        TrelloBoardDto trelloBoardDto2 = new TrelloBoardDto("2","test board2", testLstTrelloListDto);
+        TrelloBoardDto trelloBoardDto3 = new TrelloBoardDto("3","test board3", testLstTrelloListDto);
+        List<TrelloBoardDto> testLstTrelloBoardsDto = new ArrayList<>();
+        testLstTrelloBoardsDto.add(trelloBoardDto1);
+        testLstTrelloBoardsDto.add(trelloBoardDto2);
+        testLstTrelloBoardsDto.add(trelloBoardDto3);
+        TrelloCardDto testTrelloCardDto = new TrelloCardDto("test card name", "test description", "top", "1");
 
         //When
-        List<TrelloBoard> lstTrelloBoards = trelloMapper.mapToBoards(lstTrelloBoardsDto);
+        List<TrelloBoard> lstTrelloBoards = trelloMapper.mapToBoards(testLstTrelloBoardsDto);
+        List<TrelloBoardDto> lstTrelloBoardsDto = trelloMapper.mapToBoardsDto(lstTrelloBoards);
+        List<TrelloList> lstTrelloList = trelloMapper.mapToList(testLstTrelloListDto);
+        List<TrelloListDto> lstTrelloListDto = trelloMapper.mapToListDto(lstTrelloList);
+        TrelloCard trelloCard = trelloMapper.mapToTrelloCard(testTrelloCardDto);
+        TrelloCardDto trelloCardDto = trelloMapper.mapToTrelloCardDto(trelloCard);
 
         //Then
         Assert.assertEquals(lstTrelloBoards.get(2).getId(), "3");
+        Assert.assertEquals(lstTrelloBoards.get(0).getName(), "test board1");
+        Assert.assertEquals(lstTrelloBoardsDto.get(1).getId(), "2");
+        Assert.assertEquals(lstTrelloBoardsDto.get(2).getName(), "test board3");
+        Assert.assertEquals(lstTrelloList.get(0).getId(), "1");
+        Assert.assertEquals(lstTrelloList.get(0).getName(), "test list name");
+        Assert.assertEquals(lstTrelloListDto.get(0).getId(), "1");
+        Assert.assertEquals(lstTrelloListDto.get(0).getName(), "test list name");
+        Assert.assertEquals(trelloCard.getDescription(), "test description");
+        Assert.assertEquals(trelloCard.getName(), "test card name");
+        Assert.assertEquals(trelloCardDto.getDescription(), "test description");
+        Assert.assertEquals(trelloCardDto.getName(), "test card name");
     }
 }
