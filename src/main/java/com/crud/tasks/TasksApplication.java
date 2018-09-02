13331523f596e126e067fb2cb273ee916d21c5a2
Karol <kb.appsdesign@gmail.com>
2018-09-02 13:11:00 +0200

    Settings restored from Heroku to local application

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+/* HEROKU
 @SpringBootApplication
 public class TasksApplication {
 
@@ -13,11 +14,10 @@ public class TasksApplication {
 	}
 
 }
+*/
 
 
-/* VERSION FOR LOCAL TOMCAT SERVER
-
-
+@SpringBootApplication
 public class TasksApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
@@ -30,4 +30,3 @@ public class TasksApplication extends SpringBootServletInitializer {
 	}
 
 }
-*/
