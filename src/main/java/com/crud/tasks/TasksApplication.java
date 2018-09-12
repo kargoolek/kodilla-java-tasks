07bad87554c7d5817b4d3afac2f85d921f6d9d5e
Karol <kb.appsdesign@gmail.com>
2018-09-12 21:37:53 +0200

    24.1. Exercise finished. Thymeleaf variables.

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-// HEROKU
+/* HEROKU
 @SpringBootApplication
 public class TasksApplication {
 
@@ -14,9 +14,9 @@ public class TasksApplication {
 	}
 
 }
+*/
 
-
-/*LOCAL APP
+//LOCAL APP
 @SpringBootApplication
 public class TasksApplication extends SpringBootServletInitializer {
 
@@ -29,5 +29,5 @@ public class TasksApplication extends SpringBootServletInitializer {
 		return application.sources(TasksApplication.class);
 	}
 
-} */
+} //
 
