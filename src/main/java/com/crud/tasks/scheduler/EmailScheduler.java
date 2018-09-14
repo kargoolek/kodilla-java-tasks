104fd43d27a8f47ad1467c5812fedfaaecc69918
Karol <kb.appsdesign@gmail.com>
2018-09-14 11:01:11 +0200

    24.3. Exercise finished. Create template for once per day mail message.

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class EmailScheduler {
     private static final String SUBJECT = "Tasks: Once a day email";
+    public static final String DAILY_TASKS_MESSAGE = "Currently in database you got: ";
 
     @Autowired
     private SimpleEmailService simpleEmailService;
@@ -21,13 +22,14 @@ public class EmailScheduler {
     @Autowired
     private AdminConfig adminConfig;
 
-    @Scheduled(cron = "0 0 20 * * *")
+    @Scheduled(cron = "0 0 20 * * *", zone = "CEST")
+    @Scheduled(fixedDelay = 10000)
     //@Scheduled(fixedDelay = 10000)
     public void sendInformationEmail() {
         long size = taskRepository.count();
         String tasks = (size == 1) ? " task." : " tasks.";
         simpleEmailService.send(new Mail(
-            adminConfig.getAdminMail(), "", SUBJECT, "Currently in database you got: " + size + tasks
+            adminConfig.getAdminMail(), "", SUBJECT, DAILY_TASKS_MESSAGE + size + tasks
         ));
     }
 
--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,12 +1,18 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.domain.TrelloBoard;
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.scheduler.EmailScheduler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class MailCreatorService {
@@ -23,6 +29,9 @@ public class MailCreatorService {
     @Value("${info.company.phone}")
     String companyPhone;
 
+    @Value("${trello.app.username}")
+    String trelloUserName;
+
     @Autowired
     private AdminConfig adminConfig;
 
@@ -30,7 +39,22 @@ public class MailCreatorService {
     @Qualifier("templateEngine")
     TemplateEngine templateEngine;
 
+    @Autowired
+    TrelloService trelloService;
+
     public String buildTrelloCardEmail(String message) {
+
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
+        List<String> boardsNames = new ArrayList<>();
+        List<TrelloBoardDto> boards = trelloService.fetchTrelloBoards();
+        for (TrelloBoardDto b : boards) {
+            boardsNames.add(b.getName());
+        }
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "https://kargoolek.github.io/");
@@ -42,9 +66,19 @@ public class MailCreatorService {
         context.setVariable("company_phone", companyPhone);
         context.setVariable("goodbye_message", "See you again! :)");
         context.setVariable("preview_message", "Message from Trello Mail Service.");
+        context.setVariable("show_button", false);
+        context.setVariable("isFriend", false);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("application_functionality", functionality);
+
+        //Trello once per day mail
+        context.setVariable("once_per_day_trello_boards", boardsNames);
+        context.setVariable("once_per_day_trello_link", "www.trello.com/"+trelloUserName+"/boards");
+        context.setVariable("once_per_day_button_show", boardsNames.size() > 0);
+        context.setVariable("once_per_day_button_text", "SHOW ALL BOARDS");
+
+        if(message.contains(EmailScheduler.DAILY_TASKS_MESSAGE)) return templateEngine.process("mail/daily-tasks-count-mail", context);
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
-
-
 
 }
